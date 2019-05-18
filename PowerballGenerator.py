dd23fb1bee83979a634c108b47836c61469a6ab0
Osama A <41015632+codingXllama@users.noreply.github.com>
2019-05-17 23:34:06 -0600

    Adding comments to indicate what the programs does

--- PowerballGenerator.py
+++ PowerballGenerator.py
@@ -1,13 +1,16 @@
 import random, time
 
+#finding the start time of the program
 startTime = time.time()
 
+#creating an emptyList to store the values of the random generated numbers
 emptyList = []
 for number in range(10000):
     ranodmNumber = random.randint(1, 71)
 
     emptyList.append(ranodmNumber)
 
+#printing to the user what the random numbers are
 print("The list is: ", emptyList)
 
 # finding the frequency of each element in the list
@@ -19,15 +22,18 @@ for item in emptyList:
     else:
         numberFreQ[item] = 1
 
+ #printing the keys and the number of times it appears onto a file
 for key, value in numberFreQ.items():
     # print("%d : %d" % (key, value))
     myFile.write("{}: {} \n".format(key, value))
 
-
+#finding the max number of keys in the file
 key = max(numberFreQ, key=lambda k: numberFreQ[k])
 myFile.write("The most frequent number is:{}".format(key))
 endTime = time.time()
+#calculating the total time for running the program 
 finalTime = endTime - startTime
 print("final time taken", finalTime)
 
+#closing the file stream
 myFile.close()
